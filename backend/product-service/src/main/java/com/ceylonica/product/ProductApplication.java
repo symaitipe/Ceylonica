b67c05220e8084fb5ad480ed683c958f6770915d
Avishka Wijesinghe <avishkapraveen02@gmail.com>
2026-02-03 23:19:31 +0530

    refactor

--- backend/product-service/src/main/java/com/ceylonica/product/ProductApplication.java
+++ backend/product-service/src/main/java/com/ceylonica/product/ProductApplication.java
@@ -2,10 +2,10 @@ package com.ceylonica.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+//import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
+// @EnableMongoAuditing
 public class ProductApplication {
 
 	public static void main(String[] args) {
