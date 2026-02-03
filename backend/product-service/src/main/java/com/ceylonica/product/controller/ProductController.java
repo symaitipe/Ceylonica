22e9592f4117480448355ad6f2b60cdcd4b582de
Avishka Wijesinghe <avishkapraveen02@gmail.com>
2026-02-03 15:28:30 +0530

    refactor

--- backend/product-service/src/main/java/com/ceylonica/product/controller/ProductController.java
+++ backend/product-service/src/main/java/com/ceylonica/product/controller/ProductController.java
@@ -1,5 +1,27 @@
 package com.ceylonica.product.controller;
 
+import com.ceylonica.product.model.ProductDTO;
+import com.ceylonica.product.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ProductController {
 
+    @Autowired
+    ProductServiceImpl productService;
+
+    @PostMapping(value = "/api/addproduct")
+    public ProductDTO addProduct(@RequestBody ProductDTO productDTO)
+    {
+        return productService.addProduct(productDTO);
+    }
+
+    @GetMapping(value = "/api/product/{id}")
+    public ProductDTO getproduct(@PathVariable Integer productId)
+    {
+        return productService.getByProductId(productId);
+    }
+
+    
 }
