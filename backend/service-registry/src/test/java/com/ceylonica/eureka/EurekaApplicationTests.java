1a18b71ceb46951deae5ef01ce03a77444fe4627
ksdeshappriya <ksdeshappriya.official@gmail.com>
2026-02-15 11:08:56 +0530

    feat: configuration update

---
+++ backend/service-registry/src/test/java/com/ceylonica/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceylonica.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
